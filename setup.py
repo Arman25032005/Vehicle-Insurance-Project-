8f11c572e9f665c50afdff943307f90754f5b369
Arman25032005 <chaturvediarman2005@gmail.com>
2026-03-01 18:33:33 +0530

    project structure is ready and virtual environment is created and mongodb setup is done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Arman Chaturvedi",
+    author_email="chaturvediarman2005@gmail.com",
+    packages=find_packages()
+)
